1818dec2fe8b40a208f463978da8cb7bfc2fa2aa
Antrax <fredcox@gmail.com>
2025-07-21 16:57:45 -0300

    ok

--- 0-install.sh
+++ 0-install.sh
@@ -1,18 +1,30 @@
 #!/usr/bin/env bash
 
+pause() {
+  read -n 1 -s -r -p "Pressione qualquer tecla para continuar..."
+}
+
 echo "Updating xbps and the System..."
 sudo xbps-install -S
 sudo xbps-install -S xbps
 sudo xbps-install -Syu
 
+pause
+
+echo "Installing applications and drivers..."
 sudo xbps-install unzip wget curl nfs-utils sv-netmount mesa-dri xorg-minimal \
   vulkan-loader mesa-vulkan-radeon mesa-vaapi mesa-vdpau void-repo-nonfree dbus seatd
 
+pause
+
+echo "NFS on fstab..."
 sudo mkdir /NFS
 sudo chown -R antrax:antrax /NFS
 
 echo "Write /etc/fstab"
 echo "192.168.1.24:/ /NFS nfs rw,hard 0 0" | sudo tee -a /etc/fstab
+
+pause
 
 echo "Enable services statd, rpcbind, dbus and seatd and netmount..."
 sudo ln -s /etc/sv/dbus /var/service
@@ -20,6 +32,8 @@ sudo ln -s /etc/sv/seatd /var/service
 sudo ln -s /etc/sv/statd /var/service
 sudo ln -s /etc/sv/rpcbind /var/service
 sudo ln -s /etc/sv/netmount /var/service
+
+pause
 
 echo "Install nerd-fonts....."
 ./nerd-fonts.sh Hack
