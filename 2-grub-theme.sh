fb645934ba64fbf1ef09b726dcfcc04df37172a7
Antrax <fredcox@gmail.com>
2025-07-21 18:26:12 -0300

    ok

--- 2-grub-theme.sh
+++ 2-grub-theme.sh
@@ -2,8 +2,9 @@
 
 echo "Install grub theme..."
 
-sudo mkdir -p /boot/grub/themes
-sudo tar -xf void-linux.tar -C /boot/grub/themes/
+sudo mkdir -p /boot/grub/themes/void-linux
+sudo tar -xf void-linux.tar -C /boot/grub/themes/void-linux
 echo 'GRUB_THEME=/boot/grub/themes/void-linux/theme.txt' | sudo tee -a /etc/default/grub
+sudo grub-mkconfig -o /boot/grub/grub.cfg
 
 echo "OK"
