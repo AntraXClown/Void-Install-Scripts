8c994af70a1541b8f471de642435122ae9840d4f
Antrax <fredcox@gmail.com>
2025-07-21 12:13:30 -0300

    ok

--- install-nerd-fonts.sh
+++ install-nerd-fonts.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-sudo xbps-install unzip
-./nerd-fonts.sh Hack 
-./nerd-fonts.sh CascadiaCode 
-./nerd-fonts.sh JetBrainsMono 
-./nerd-fonts.sh Meslo 
-./nerd-fonts.sh UbuntuMono 
+sudo xbps-install unzip wget curl
+./nerd-fonts.sh Hack
+./nerd-fonts.sh CascadiaCode
+./nerd-fonts.sh JetBrainsMono
+./nerd-fonts.sh Meslo
+./nerd-fonts.sh UbuntuMono
 ./nerd-fonts.sh FiraCode
