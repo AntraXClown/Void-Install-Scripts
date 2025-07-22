bd5d0baf72e40f6cd6223217cab931c41cb5f873
antrax <fredcox@gmail.com>
2025-07-22 07:54:49 -0300

    polkit-kde-agent

--- packages.sh
+++ packages.sh
@@ -7,7 +7,7 @@ PACKAGES=(
   swappy wl-clipboard slurp CopyQ duf bat kdenlive
   telegram-desktop evince net-tools btop 7zip stow
   entr ristretto man-db tldr cava gtk4-layer-shell calibre
-  alacritty firefox base-devel
+  alacritty firefox base-devel pavucontrol-qt polkit-kde-agent
   ##########################################################################################
   # zsh
   ##########################################################################################
